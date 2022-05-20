678fd1ddbc54549a766f532c64ba86d2a0472ee9
penne8 <paney@post.bgu.ac.il>
2022-05-20 14:04:06 +0300

    task 1 - Full adder

---
+++ Assignment-2/ex2.pl
@@ -0,0 +1,128 @@
+/* I, Pan Eyal (208722058) assert that the work I submitted is entirely my own.
+I have not received any part from any other student in the class (or other source),
+nor did I give parts of it for use to others. I have clearly marked in the comments
+of my program any code taken from an external source. */
+
+user:file_search_path(xxxx, 'C:/Users/paney/Documents/Studies/Logic_Programming_Class/Assignment-2/satsolver').
+:- use_module(xxxx(satsolver)).
+
+
+/* ---------------------------- TASK 1 ---------------------------- */
+
+/*
+---- Full Adder Truth Table ----
+    X  Y Cin |  Z Cout | Value
+_____________|_________|________
+    0  0  0  |  0   0  |   T
+    0  0  0  |  1   0  |   F
+    0  0  0  |  0   1  |   F
+    0  0  0  |  1   1  |   F
+             |         |
+    1  0  0  |  0   0  |   F
+    1  0  0  |  1   0  |   T
+    1  0  0  |  0   1  |   F
+    1  0  0  |  1   1  |   F
+             |         |
+    0  1  0  |  0   0  |   F
+    0  1  0  |  1   0  |   T
+    0  1  0  |  0   1  |   F
+    0  1  0  |  1   1  |   F
+             |         |
+    1  1  0  |  0   0  |   F
+    1  1  0  |  1   0  |   F
+    1  1  0  |  0   1  |   T
+    1  1  0  |  1   1  |   F
+             |         |
+    0  0  1  |  0   0  |   F
+    0  0  1  |  1   0  |   T
+    0  0  1  |  0   1  |   F
+    0  0  1  |  1   1  |   F
+             |         |
+    1  0  1  |  0   0  |   F
+    1  0  1  |  1   0  |   F
+    1  0  1  |  0   1  |   T
+    1  0  1  |  1   1  |   F
+             |         |
+    0  1  1  |  0   0  |   F
+    0  1  1  |  1   0  |   F
+    0  1  1  |  0   1  |   T
+    0  1  1  |  1   1  |   F
+             |         |
+    1  1  1  |  0   0  |   F
+    1  1  1  |  1   0  |   F
+    1  1  1  |  0   1  |   F
+    1  1  1  |  1   1  |   T
+             |         |
+
+By negating all the false values rows, we can convert from DNF to CNF.
+Therefore the CNF for the full adder is:
+
+*/
+full_adder(X, Y, Cin, Z, Cout, CNF) :-
+    CNF =  [[ X, Y, Cin,-Z, Cout],
+            [ X, Y, Cin, Z,-Cout],
+            [ X, Y, Cin,-Z,-Cout],
+
+            [-X, Y, Cin, Z, Cout],
+            [-X, Y, Cin, Z,-Cout],
+            [-X, Y, Cin,-Z,-Cout],
+
+            [ X,-Y, Cin, Z, Cout],
+            [ X,-Y, Cin, Z,-Cout],
+            [ X,-Y, Cin,-Z,-Cout],
+
+            [-X,-Y, Cin, Z, Cout],
+            [-X,-Y, Cin,-Z, Cout],
+            [-X,-Y, Cin,-Z,-Cout],
+
+            [ X, Y,-Cin, Z, Cout],
+            [ X, Y,-Cin, Z,-Cout],
+            [ X, Y,-Cin,-Z,-Cout],
+
+            [-X, Y,-Cin, Z, Cout],
+            [-X, Y,-Cin,-Z, Cout],
+            [-X, Y,-Cin,-Z,-Cout],
+
+            [ X,-Y,-Cin, Z, Cout],
+            [ X,-Y,-Cin,-Z, Cout],
+            [ X,-Y,-Cin,-Z,-Cout],
+
+            [-X,-Y,-Cin, Z, Cout],
+            [-X,-Y,-Cin,-Z, Cout],
+            [-X,-Y,-Cin, Z,-Cout]].
+
+% Base case for X=0, Y=0
+add([-1], [-1], Cin, [Z,Cout], CNF) :-
+    full_adder(-1, -1, Cin, Z, Cout, CNF).
+
+% Case for X=0 Y!=0
+add([-1], [Y|Ys], Cin, [Z|Zs], CNF) :-
+    full_adder(-1, Y, Cin, Z, Cout, CNF1),
+    add([-1], Ys, Cout, Zs, CNF2),
+    append(CNF1, CNF2, CNF).
+
+% Case for X!=0 Y=0
+add([X|Xs], [-1], Cin, [Z|Zs], CNF) :-
+    full_adder(X, -1, Cin, Z, Cout, CNF1),
+    add(Xs, [-1], Cout, Zs, CNF2),
+    append(CNF1, CNF2, CNF).
+
+% Case for X!=0 Y!=0
+add([X|Xs], [Y|Ys], Cin, [Z|Zs], CNF) :-
+    full_adder(X, Y, Cin, Z, Cout, CNF1),
+    add(Xs, Ys, Cout, Zs, CNF2),
+    append(CNF1, CNF2, CNF).
+
+add(Xs, Ys, Zs, CNF) :-
+    add(Xs, Ys, -1, Zs, CNF).
+
+/* ---------------------------- TASK 2 ---------------------------- */
+
+pad(Ys, Ys).
+pad(Ys, PaddedYs) :-
+    append(Ys,[-1],PaddedYs1),
+    pad(PaddedYs1, PaddedYs).
+
+leq(Xs,Ys,CNF) :-
+    pad(Ys, PaddedYs),
+    add(Xs, _, -1, PaddedYs, CNF).
