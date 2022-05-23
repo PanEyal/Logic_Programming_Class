6b8dff4f4f9dc657703ea0c52c481afcb2942148
penne8 <paney@post.bgu.ac.il>
2022-05-23 23:54:57 +0300

    kinda work, some duplicates

--- Assignment-2/ex2.pl
+++ Assignment-2/ex2.pl
@@ -152,3 +152,42 @@ sum(LON, Zs, CNF) :-
     sum([-1], LON, Zs, CNF).
 
 /* ---------------------------- TASK 4 ---------------------------- */
+
+% base case for multiplying with zero.
+times(Zero, _Y, [-1], CNF) :- leq(Zero, [-1], CNF).
+
+% for 0 LSBF multiply last calucaltion by 2.
+times([-1|Xs], Ys, Zs, CNF) :-
+    lt([-1],Xs,CNF1),
+    times(Xs, Ys, PREV_Z, CNF2),
+    add(PREV_Z, PREV_Z, Zs, CNF3),
+    append(CNF1, CNF2, CNF12),
+    append(CNF12, CNF3, CNF).
+
+% for 1 LSBF calculate like 0 is the LSBF and then add Y.
+times([1|Xs], Ys, Zs, CNF) :-
+    times([-1|Xs], Ys, PREV_Zs, CNF1),
+    add(PREV_Zs, Ys, Zs, CNF2),
+    append(CNF1, CNF2, CNF).
+
+/* ---------------------------- TASK 5 ---------------------------- */
+
+/*
+Task 1
+    Xs=[1,_], Ys=[_,_,_], add(Xs,Ys,Zs,Cnf), sat(Cnf).
+    Zs=[1,-1,1,1], PaddedZs=[1,-1,1,1,-1], length(Xs,4), length(Ys,4), add(Xs,Ys,PaddedZs,Cnf), sat(Cnf).
+Task 2
+    Xs=[1,_,_], Ys=[_,_], leq(Xs,Ys,Cnf), sat(Cnf).
+    Xs=[1,_,_], Ys=[_,_], lt(Xs,Ys,Cnf), sat(Cnf).
+Task 3
+    Xs1 = [-1, -1, -1, 1, -1], Xs2 = [-1, 1, -1, -1, -1], Xs3 = [1, -1, -1, -1, -1], sum([Xs1,Xs2,Xs3],Zs,Cnf), sat([Xs1,Xs2,Xs3|Cnf]).
+    Zs = [-1,1,1,1,1], PaddedZs= [-1,1,1,1,1,-1,-1,-1], List = [Xs1, Xs2, Xs3], length(Xs1,5), length(Xs2,5), length(Xs3,5), sum(List,PaddedZs,Cnf), sat(Cnf).
+Task 4
+    Xs=[_,_], Ys=[_,_,_], times(Xs,Ys,Zs,Cnf),sat([Xs,Ys|Cnf]).
+    Zs=[1,1,1,1], PaddedZs=[1,1,1,1,-1,-1,-1,-1],length(Xs,4), length(Ys,4), times(Xs,Ys,PaddedZs,Cnf), sat(Cnf).
+
+length(Xs, Xs_Size), length(Ws, Ws_Size), pad(Xs, (Ws_Size - Xs_Size) + 1, PaddedXs), add([1], Ws, PaddedXs, CNF1),
+
+length(Xs, Xs_Size), length(Zs, Zs_Size), pad(Xs, (Zs_Size - Xs_Size) + 1, PaddedXs), add([1], Zs, PaddedXs, CNF)
+
+*/
