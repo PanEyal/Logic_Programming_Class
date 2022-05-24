0c5b92bd841acae6a772cb5fb8956855c4892cda
penne8 <paney@post.bgu.ac.il>
2022-05-25 00:04:02 +0300

    other times implementation

--- Assignment-2/ex2.pl
+++ Assignment-2/ex2.pl
@@ -181,6 +181,23 @@ times(Xs, Ys, Zs, CNF) :-
     sum(LON, Zs, CNF2),
     append(CNF1, CNF2, CNF).
 
+% % base case for multiplying with zero.
+% times(Zero, _Y, [-1], CNF) :- leq(Zero, [-1], CNF).
+
+% % for 0 LSBF multiply last calucaltion by 2.
+% times([-1|Xs], Ys, Zs, CNF) :-
+%     lt([-1],Xs,CNF1),
+%     times(Xs, Ys, PREV_Z, CNF2),
+%     add(PREV_Z, PREV_Z, Zs, CNF3),
+%     append(CNF1, CNF2, CNF12),
+%     append(CNF12, CNF3, CNF).
+
+% % for 1 LSBF calculate like 0 is the LSBF and then add Y.
+% times([1|Xs], Ys, Zs, CNF) :-
+%     times([-1|Xs], Ys, PREV_Zs, CNF1),
+%     add(PREV_Zs, Ys, Zs, CNF2),
+%     append(CNF1, CNF2, CNF).
+
 /* ---------------------------- TASK 5 ---------------------------- */
 
 /*
