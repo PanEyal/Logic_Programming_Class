2699095303eb94e9aa9f31e8eb2bae090c9a6ac7
Pan Eyal <paney@post.bgu.ac.il>
2022-06-13 19:25:15 +0300

    Ass 3 - Skeleton files

---
+++ Assignment-3/ex3.pl
@@ -0,0 +1,9 @@
+
+user:file_search_path(sat, './satsolver').
+user:file_search_path(bee, './beeCompiler').
+
+:- use_module(sat(satsolver)).
+:- use_module(bee(bCompiler)).
+:- dynamic listKeepFrom/4.
+
+    
