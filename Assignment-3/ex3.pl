0837d1702a72b1b897b0eabe300cb3e8c0c5ebf6
penne8 <paney@post.bgu.ac.il>
2022-06-14 01:42:27 +0300

    WIP

--- Assignment-3/ex3.pl
+++ Assignment-3/ex3.pl
@@ -32,4 +32,36 @@ kakuroVerify([], []) :-
 kakuroVerify([Element|Rest1], [Element|Rest2]) :-
     (kakuroVerifyElement(Element) -> kakuroVerify(Rest1, Rest2); writeln(verify:wrong)).
 
-    
+% ------------------------------- Encode ------------------------------- %
+
+kakuroDeclareBlockInts([], []).
+kakuroDeclareBlockInts([N|Rest], Constraints) :-
+    kakuroDeclareBlockInts(Rest, Cs),
+    (not(var(N)) -> append([[new_int(N, 1, 9)], Cs], Constraints); Cs = Constraints),!.
+
+kakuroEncode([], []).
+kakuroEncode([(ClueSum=Block)|Rest], Constraints) :-
+    Cs1 = [new_int(ClueSumDeclared, ClueSum, ClueSum), bool_array_sum_eq(Block, ClueSumDeclared)],
+    kakuroDeclareBlockInts(Block, Cs2),
+    kakuroEncode(Rest, Cs3),
+    append([Cs1, Cs2, Cs3], Constraints),!.
+
+kakuroEncode(Instance, Instance, Constraints) :-
+    kakuroEncode(Instance, Constraints),!.
+
+% ------------------------------- Decode ------------------------------- %
+
+kakuroDecode([], []).
+kakuroDecode([(ClueSum=Block1)|Rest1],[(ClueSum=Block2)|Rest2]):-
+    % use BEE auxiliary module to decode integers
+    bDecode:decodeIntArray(Block1, Block2),
+    kakuroDecode(Rest1, Rest2).
+
+% -------------------------------- Solve -------------------------------- %
+
+kakuroSolve(Instance,Solution):-
+    kakuroEncode(Instance, Map, Constraints),
+    bCompile(Constraints, Cnf),
+    sat(Cnf).
+    % kakuroDecode(Map, Solution),
+    % (kakuroVerify(Instance, Solution) -> true ; writeln(wrong)).
